42fa25c6b9a926b56deabc3f046e2f081d40e20b
monkeywaitme <3306091@qq.com>
2020-12-23 22:52:48 +0800

    希尔排序

---
+++ src/main/java/com/example/base/数据结构和算法/书籍/排序/高级排序/ShellSort.java
@@ -0,0 +1,38 @@
+package com.example.base.数据结构和算法.书籍.排序.高级排序;
+
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        long[] theArray = {33, 66, 22, 77, 55, 11, 99, 44, 88};
+        shellSort(theArray);
+        for (int j = 0; j < theArray.length; j++) {
+            System.out.println(theArray[j] + " ");
+        }
+        System.out.println("");
+    }
+
+    public static void shellSort(long[] arr) {
+        int inner, outer;
+        long temp;
+
+        int h = 1;
+        while (h < (arr.length / 3)) {
+            h = h * 3 + 1;
+        }
+        while (h > 0) {
+            for (outer = h; outer < arr.length; outer++) {
+                temp = arr[outer];
+                inner = outer;
+
+                while (inner > h - 1 && arr[inner - h] >= temp) {
+                    arr[inner] = arr[inner - h];
+                    inner -= h;
+                }
+                arr[inner] = temp;
+            }
+            h = (h - 1) / 3;
+        }
+    }
+
+}
