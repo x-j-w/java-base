4556f616f49498731b31d29977c60fcd29602d59
jiwei.xue <3306091@qq.com>
2020-12-30 14:09:10 +0800

    avl 平衡二叉树 添加链接

--- src/main/java/com/example/base/数据结构和算法/书籍/树/平衡二叉树avl/AvlTree.java
+++ src/main/java/com/example/base/数据结构和算法/书籍/树/平衡二叉树avl/AvlTree.java
@@ -81,9 +81,33 @@ public class AvlTree<E extends Comparable<E>> {
 
 
     /**
+     * 查找节点
+     *
+     * @param e
+     * @return
+     */
+    public Node getNode(E e) {
+        Node current = root;
+
+        while (current.e != e) {
+            if (e.compareTo(current.e) < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+            if (current == null) {
+                return null;
+            }
+        }
+        return current;
+    }
+
+
+
+    /**
      *   LL的意思是向左子树（L）的左孩子（L）中插入新节点后导致不平衡，这种情况下需要右旋操作，而不是说LL的意思是右旋，后面的也是一样。
      * 右旋转
-     * 自己变为左孩子的右孩子,自己的左孩子的有孩子变为自己的左孩子
+     * 自己变为左孩子的右孩子,自己的左孩子的右孩子变为自己的左孩子
      * @return
      */
     private Node rightRotate(Node y) {
@@ -97,6 +121,13 @@ public class AvlTree<E extends Comparable<E>> {
         return x;
     }
 
+    /**
+     *  LL的意思是向左子树（L）的左孩子（L）中插入新节点后导致不平衡，这种情况下需要右旋操作，而不是说LL的意思是右旋，后面的也是一样。
+     *  左旋转
+     *  自己变为右孩子的左孩子,自己的右孩子的左孩子变为自己的右孩子
+     * @param y
+     * @return
+     */
     private Node leftRotate(Node y) {
         Node x = y.right;
         Node t2 = x.left;
@@ -107,6 +138,142 @@ public class AvlTree<E extends Comparable<E>> {
         x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
         return x;
     }
+
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    public Node add(Node node, E e) {
+        if(node == null){
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else {
+            node.right = add(node.right, e);
+        }
+
+        // 更新height
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(node);
+        if (balanceFactor > 1 && getBalanceFactor(node.left ) > 0) {
+            //右旋
+            return rightRotate(node);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(node.right) < 0) {
+            // 左旋
+            return leftRotate(node);
+        }
+        // LR
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        // RL
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+        return node;
+    }
+
+    public E remove(E e){
+        Node node = getNode(e);
+        if(node != null){
+            root = remove(root, e);
+            return node.e;
+        }
+        return null;
+    }
+
+
+    private Node remove(Node node, E e){
+
+        if( node == null ) {
+            return null;
+        }
+        Node retNode;
+        if( e.compareTo(node.e) < 0 ){
+            node.left = remove(node.left , e);
+            retNode = node;
+        } else if(e.compareTo(node.e) > 0 ){
+            node.right = remove(node.right, e);
+            retNode = node;
+        } else {   // e.compareTo(node.e) == 0
+            // 待删除节点左子树为空的情况
+            if(node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                retNode = rightNode;
+            }
+            // 待删除节点右子树为空的情况
+            else if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                retNode = leftNode;
+            }else {
+                // 待删除节点左右子树均不为空的情况
+                // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+                // 用这个节点顶替待删除节点的位置
+                Node successor = minimum(node.right);
+                successor.right = remove(node.right, successor.e);
+                successor.left = node.left;
+
+                node.left = node.right = null;
+
+                retNode = successor;
+            }
+        }
+        if (retNode == null) {
+            return null;
+        }
+        //维护平衡
+        //更新height
+        retNode.height = 1+Math.max(getHeight(retNode.left),getHeight(retNode.right));
+        //计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+        if(balanceFactor > 1 && getBalanceFactor(retNode.left)>=0) {
+            //右旋LL
+            return rightRotate(retNode);
+        }
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right)<=0) {
+            //左旋RR
+            return leftRotate(retNode);
+        }
+        //LR
+        if(balanceFactor > 1 && getBalanceFactor(retNode.left) < 0){
+            node.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
+        }
+        //RL
+        if(balanceFactor < -1 && getBalanceFactor(retNode.right) > 0){
+            node.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
+        }
+        return retNode;
+    }
+
+    /**
+     * 查找最小节点
+     *
+     * @return
+     */
+    public Node minimum(Node node ) {
+        Node current = node;
+        Node last = null;
+        while (current != null) {
+            last = current;
+            current = current.left;
+        }
+        return last;
+    }
+
 
 }
 
